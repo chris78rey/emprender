c810694cf71a1a66314220ce44c85fb0f5ec7787
CHRISTIAN <cris_78_rey@hotmail.com>
2019-08-04 11:37:48 -0500

    SE ESTA INICIANDO EL PROYECTO

---
+++ DIRECTORIO DE TRABAJO/01 INICIO/ideas pricipales.mm
@@ -0,0 +1,16 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1564936025079" ID="ID_1615962230" MODIFIED="1564936365946">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      PROYECTO RENTABLE FAMILIA RUIZ TELLO
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</map>
